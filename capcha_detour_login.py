6ce8626e8d77ff2e0909a61bf12cc09f02fb0adc
osori-magu <hyeseong43@gmail.com>
2020-10-29 17:43:08 +0900

    pyperclip과 selenium을 이용한 로그인 성공

---
+++ capcha_detour_login.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time 
+from selenium.webdriver.common.keys import Keys 
+import pyperclip
+
+driver = webdriver.Chrome()
+driver.get('https://www.naver.com')
+time.sleep(1)
+
+# 메인 화면에서 로그인 버튼 클릭
+login_btn = driver.find_element_by_class_name('link_login')
+login_btn.click()
+time.sleep(1)
+
+naver_id = driver.find_element_by_name('id')
+naver_pw = driver.find_element_by_name('pw')
+
+naver_id.click()
+pyperclip.copy('lhs4859')
+naver_id.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+
+naver_pw.click()
+pyperclip.copy('triumph2@1')
+naver_pw.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+
+login_btn = driver.find_element_by_id('log.login')
+login_btn.click()
